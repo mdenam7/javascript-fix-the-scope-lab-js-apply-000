13da164743e1be113f3584d9afb44f5b8510756e
md Enam <russell.m2006@gmail.com>
2018-04-07 16:56:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,10 +15,7 @@ function yourAnimal() {
 
 function add2(n) {
   return  n + two
-  
-
   // Feel free to move things around!
-
 }
 
 var funkyFunction = function() {
@@ -30,7 +27,7 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+var theFunk = funkyFunction()()
   
   
 
